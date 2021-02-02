3399629e7a65302ee979e9b702dece13f0b183c5
emmanuelopoku <emmanuel.opoku@trojans.dsu.edu>
2021-02-02 10:31:04 -0600

    adding original files

---
+++ SimpleHeartBeatService/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Topshelf;
+
+namespace SimpleHeartBeatService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var exitCode = HostFactory.Run(x =>
+            {
+                x.Service<Heartbeat>(s =>
+                {
+                    s.ConstructUsing(heartbeat => new Heartbeat());
+                    s.WhenStarted(heartbeat => heartbeat.Start());
+                    s.WhenStopped(heartbeat => heartbeat.Stop());
+                });
+
+                x.RunAsLocalSystem();
+
+                x.SetServiceName("HeartBeatService");
+                x.SetDisplayName("Heartbeat Service");
+                x.SetDescription("This is a sample heartbeat services for my ETL presentation. It should create a new test file and append the datetime every second");
+            });
+
+            int exitCodeValue = (int)Convert.ChangeType(exitCode, exitCode.GetTypeCode());
+            Environment.ExitCode = exitCodeValue;
+        }
+    }
+}
